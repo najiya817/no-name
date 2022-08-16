4e4d52d764d71aedd17074822af81035b532b861
NAJIYA NASRIN <110896779+najiya817@users.noreply.github.com>
2022-08-16 14:53:12 +0530

    message

---
+++ builtin.py
@@ -0,0 +1,10 @@
+multiline="""ytyfdfd  jhfug  hgyfgj,
+khgyfgyugygyfgy fgg ygf  ygygfh,
+jgyg efbhg hg hguy gygfyggdfty,
+gfyutjhggv,
+uhgyurf"""
+print(multiline)
+print(type(multiline))
+print(len(multiline))
+print("hg" in multiline)
+print("naji" in  multiline)
---
+++ id.py
@@ -0,0 +1,8 @@
+a=[30,40]
+b=[30,40]
+c=a
+print(a is b)
+print(a is c)
+print("address of a",id(a))
+print("address of b",id(b))
+print("address of c",id(c))
